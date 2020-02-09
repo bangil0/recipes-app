de7c5956e7834f63a4d60ce9bc344d7b8b17407a
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-02-09 19:26:54 +0800

    Updates

---
+++ precache-manifest.a55ea8823f359d884c05f391b31f34e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d685e751d102bf0dd71d68e7734f9e67",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "f60302147af16923ede4",
+    "url": "/recipes-app/static/css/main.78180829.chunk.css"
+  },
+  {
+    "revision": "59486b68e1df1893dd35",
+    "url": "/recipes-app/static/js/2.d3f3fe40.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipes-app/static/js/2.d3f3fe40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60302147af16923ede4",
+    "url": "/recipes-app/static/js/main.253bdfd4.chunk.js"
+  },
+  {
+    "revision": "50e821a381bd5c22dcc0",
+    "url": "/recipes-app/static/js/runtime-main.01641bcd.js"
+  }
+]);
